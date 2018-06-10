792f914e35a90c65a91d94484f6deebfc0288046
Carlos Ortube <cool3000bo@gmail.com>
2018-06-09 22:49:16 -0400

    Creo el test readFileTestReadTxt para verificar que el archivo leer.txt pueda abrirse correctamente

--- src/test/java/AnalyzerTest.java
+++ src/test/java/AnalyzerTest.java
@@ -1,10 +1,20 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.Set;
 
 public class AnalyzerTest {
     @Test
     public void readFileTestReadTxt(){
+        String archivoTxt = "leer.txt";
+        List<Sentence> salida = Analyzer.readFile(archivoTxt);
+        assertNotNull("Verifico que el archivo exista", salida);
+    }
+    @Test
+    public void readFileTestVerifyListScoreText(){
       String archivoTxt = "leer.txt";
-      
+      List<Sentence> salida = Analyzer.readFile(archivoTxt);
+        assertEquals("Verificamos que se lea el archivo", "0 This was not as much fun as I thought it would be .", salida);
     }
 
 }
