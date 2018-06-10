73868991a473689740e49269b09ef9a037059fc3
Carlos Ortube <cool3000bo@gmail.com>
2018-06-09 22:15:31 -0400

    Se implementa el metodo readFile de la clase Analyzer.java para realizar los test requeridos

--- src/main/java/Analyzer.java
+++ src/main/java/Analyzer.java
@@ -1,16 +1,30 @@
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Analyzer {
     /*
      * Implement this method in Part 1
      */
     public static List<Sentence> readFile(String filename) {
-
+        try (BufferedReader archiveTxt = new BufferedReader(new FileReader(filename))) {
+            String lineaActual;
+            List<Sentence> sentences = new ArrayList<>();
+            while ((lineaActual = archiveTxt.readLine()) != null) {
+                String[] score = lineaActual.split(" ");
+                String texto = " ";
+                for (int i = 1; i < score.length; i++) {
+                    texto = texto + " " + score[i];
+                }
+                Sentence sentence = new Sentence(Integer.valueOf(score[0]), texto);
+                sentences.add(sentence);
+            }
+            return sentences;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
-
     }
 
     /*
