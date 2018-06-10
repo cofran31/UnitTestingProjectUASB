37a66a8a361553d57d9fb73595366c4050fadcb1
Carlos Ortube <cool3000bo@gmail.com>
2018-06-09 20:13:10 -0400

    se crea las clases a utilizar para el proyecto Analyzer.java, Scanner.java, Word.java

---
+++ src/main/java/Analyzer.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Analyzer {
+    /*
+     * Implement this method in Part 1
+     */
+    public static List<Sentence> readFile(String filename) {
+
+        return null;
+
+    }
+
+    /*
+     * Implement this method in Part 2
+     */
+    public static Set<Word> allWords(List<Sentence> sentences) {
+
+        return null;
+
+    }
+
+    /*
+     * Implement this method in Part 3
+     */
+    public static Map<String, Double> calculateScores(Set<Word> words) {
+
+        return null;
+
+    }
+}
