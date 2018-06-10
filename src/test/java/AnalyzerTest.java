0cf695e5bae95097ae1474aea39247aa336c6386
Carlos Ortube <cool3000bo@gmail.com>
2018-06-10 08:32:25 -0400

    Creo el test readFileTestVerificaListaTxtScore para verificar un valor de una fila del campo SCORE

--- src/test/java/AnalyzerTest.java
+++ src/test/java/AnalyzerTest.java
@@ -19,10 +19,11 @@ public class AnalyzerTest {
       assertEquals("Verificamos la separacion del score y texto comparandolos que sean iguales con la sentencia de prueba", 4, salida.size());
     }
     @Test
-    public void readFileTestVerifyListScoreText(){
+    public void readFileTestVerificaListaTxtScore(){
         String archivoTxt = "leer.txt";
         List<Sentence> salida = Analyzer.readFile(archivoTxt);
-        assertEquals("Verificamos la separacion del score y texto comparandolos que sean iguales con la sentencia de prueba", 4, salida.size());
+        assertEquals("Verificamos campo Score, introducioendo el numero de la linea del archivo txt", 0, salida.get(0).getScore());
     }
+
 
 }
