d70fbc09d116edb094f8ddb42e4489fe7887b89b
Carlos Ortube <cool3000bo@gmail.com>
2018-06-10 18:26:17 -0400

    Se colocan comentarios para efectuar una buena revision del docente

--- src/test/java/AnalyzerTest.java
+++ src/test/java/AnalyzerTest.java
@@ -13,12 +13,14 @@ public class AnalyzerTest {
 
     /* TEST METODO READFILE */
 
+    // Test que verifica la existencia del archivo donde se encuentran las oraciones de sentimiento. Ej: leer.txt existe en el directorio
     @Test
     public void readFileTestReadTxt(){
         String archivoTxt = "leer.txt";
         List<Sentence> salida = Analyzer.readFile(archivoTxt);
         assertNotNull("Verifico que el archivo exista", salida);
     }
+    // Test que verifica que en caso de no existir el archivo envia una lista vacia. Ej: leer1.txt no existe en el directorio
     @Test
     public void readFileTestListaVacia(){
         String archivoTxt = "leer1.txt";
@@ -26,27 +28,32 @@ public class AnalyzerTest {
         List<Sentence> sentences = new ArrayList<>();
         assertEquals("Verificamos que el archivo no pueda abrirse y compare su respuesta con una lista vacia", sentences, salida);
     }
+    // Test que verifica la cantidad de filas que tiene el archivo de texto Ej: leer.txt con 5 filas (cuenta del 0 al 4)
     @Test
     public void readFileTestCantidadFilasIngresadasTxt(){
       String archivoTxt = "leer.txt";
       List<Sentence> salida = Analyzer.readFile(archivoTxt);
-      assertEquals("Verificamos la separacion del score y texto comparandolos que sean iguales con la sentencia de prueba", 4, salida.size());
+      assertEquals("Verificamos el numero de filas que contiene el archivo de texto", 4, salida.size());
     }
+    // Test  que verifica el campo Score Ej: "1 a bilingual charmer , just like the woman who inspired it" salida = 1
     @Test
     public void readFileTestVerificaListaTxtScore(){
         String archivoTxt = "leer.txt";
         List<Sentence> salida = Analyzer.readFile(archivoTxt);
-        assertEquals("Verificamos campo Score, introducioendo el numero de la linea del archivo txt", 0, salida.get(0).getScore());
+        assertEquals("Verificamos el campo Score, introducioendo el numero de la linea del archivo txt", 1, salida.get(0).getScore());
     }
+    // Test que verifica el campo Text Ej: "1 a bilingual charmer , just like the woman who inspired it" text = "a bilingual charmer , just like the woman who inspired it"
     @Test
     public void readFileTestVerificaListaTxtText(){
         String archivoTxt = "leer.txt";
         List<Sentence> salida = Analyzer.readFile(archivoTxt);
+        String text = "a bilingual charmer , just like the woman who inspired it";
         assertEquals("Verificamos campo Texto, introducioendo el texto de la linea del archivo txt", "This was not as much fun as I thought it would be .", salida.get(0).getText());
     }
 
     /* TEST METODO ALLWORDS */
 
+    // Test que verifica el numero total de palabras encontradas Ej: leer.txt contiene 36 palabras validas
     @Test
     public void allWordsTestSize(){
         String archivoTxt = "leer.txt";
@@ -54,6 +61,7 @@ public class AnalyzerTest {
         Set<Word> words = Analyzer.allWords(salida);
         assertEquals("Obtenemos el numero total de palabras encontradas", 36, words.size());
     }
+    // Test que verifica el numero de apariciones de una palabra en el archivo de texto Ej: it aparece 2 veces
     @Test
     public void allWordsTestNumeroApariciones(){
         String archivoTxt = "leer.txt";
@@ -67,6 +75,7 @@ public class AnalyzerTest {
         }
         assertEquals("Se calcula el numero de apariciones de la palabra objeto_word en todas las oraciones", 2,  Integer.parseInt(String.valueOf(numero_apariciones)));
     }
+    // Test que verifica la puntuacion acumulativa total de la palabra en las oraciones Ej: objeto_word = "it"  calculo => (fila 0) 1 + (fila 3) 0 = 1
     @Test
     public void allWordsTestAcumulativo(){
         String archivoTxt = "leer.txt";
